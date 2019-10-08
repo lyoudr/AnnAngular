0a9b15ed89d2ed93cfc877124b6f7dac28852556
ann.ko <ann.ko@368media.com>
2019-10-08 14:04:54 +0800

    add http test

--- Ann/src/app/content/discuss/discuss.spec.ts
+++ Ann/src/app/content/discuss/discuss.spec.ts
@@ -1,14 +1,66 @@
-import {DiscussComponent} from './discuss.component';
-import {DiscussService} from '../../services/discuss.service';
-import {CookieService } from 'ngx-cookie-service';
+import { DiscussComponent} from './discuss.component';
+import { DiscussService} from '../../services/discuss.service';
+import { CookieService } from 'ngx-cookie-service';
+// Http testing module and mocking controller
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+// Other imports
+import { TestBed } from '@angular/core/testing';
 
 describe('DiscussComponent', () => {
+
     let discussService : DiscussService;
     let cookieService : CookieService;
+    
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [DiscussService],
+            imports : [HttpClientTestingModule]
+        });
+        // Inject this service to each test by calling TestBed.get() with th e service class as the argument.
+        discussService = TestBed.get(DiscussService);
+    });
 
     it('#changeChat() should change chattype', () => {
         const discusscomp = new DiscussComponent(discussService, cookieService);
-        discusscomp.changeChat('chat');
-        expect(discusscomp.chattype).toBe('chat');
+        const chattype = ['chat', 'phone', 'contact', 'notification'];
+        chattype.forEach((type) => {
+            discusscomp.changeChat(type);
+            expect(discusscomp.chattype).toBe(type);
+        });
+    });
+
+    it('#showMessage() should return right message from discussService', () => {
+        let personId = ['Json', 'Joy', 'Amy', 'Tonal'];
+        let resPonses = [
+            [
+                {name: 'Json', message: 'Hello'},
+                {name: 'me', message: 'Hi~'},
+                {name: 'Json', message: 'Where do you live ?'},
+                {name: 'me', message: 'I live in Taipei now.'}
+            ],
+            [
+                {name: 'Joy', message: "I'm sad."},
+                {name: 'me', message: 'Why?'},
+                {name: 'Joy', message: 'Cause the wether is blue.'},
+                {name: 'me', message: 'Hope I can make you happy :)'} 
+            ],
+            [
+                {name: 'Amy', message: "Hey, sister, do you wanna go shopping this weekend ?"},
+                {name: 'me', message: 'Sounds good. which shopping district do you want to go ?'},
+                {name: 'Amy', message: 'Um... maybe Ximending?'},
+                {name: 'me', message: "That's a good idea."}
+            ],
+            [
+                {name: 'Tonal', message: "How's your life in London ? "},
+                {name: 'me', message: 'Not bad, but I still try to adapt to the new environment. try to know some friends , you know...'},
+                {name: 'Tonal', message: 'It is lonely to live there alone ? sounds like you need more friend.'},
+                {name: 'me', message: "Sometimes, but I think I'll get better and better. thanks for your concern."} 
+            ]
+        ];
+        personId.forEach((person, index) => {
+            discussService.getMessage(person).subscribe(value => {
+                expect(JSON.parse(value)).toEqual(resPonses[index]);
+            });
+        });
     });
 });
